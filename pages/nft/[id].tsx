5b20ff5381bc27f5ee23b93bc4283620a8497442
jonathancsr <jonathancsrr@gmail.com>
2022-09-28 14:46:07 -0300

    feat: section more works

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,24 +2,29 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { nfts } from "..";
+import { NFTCard } from "../../components/NFTCard";
 
 export default function NFTDetails() {
   const router = useRouter();
-  const nft  = nfts.find((nft) => nft.id === router.query.id);
+  const nft = nfts.find((nft) => nft.id === router.query.id);
   console.log(nft);
   return (
     <div>
       <Head>
-        <title>{nft.name} NFT - Details Page</title>
+        <title>{nft?.name} NFT - Details Page</title>
       </Head>
 
       <div className="mt-24 flex space-x-[10%]">
-        <img className="rounded-[1.25rem] w-[45%] h-auto" src={nft?.image} alt={nft?.name}/>
-        
+        <img
+          className="rounded-[1.25rem] w-[45%] h-auto"
+          src={nft?.image}
+          alt={nft?.name}
+        />
+
         <div className="ml-10 w-[45%]">
           <h1 className="text-5xl font-bold">{nft?.name}</h1>
           <p className="text-[#93989A] mt-4">{nft?.description}</p>
-          
+
           <hr className="w-full border-[#242634] mt-8 mb-4" />
 
           <div>
@@ -28,12 +33,22 @@ export default function NFTDetails() {
           </div>
 
           <hr className="w-full border-[#242634] mt-8 mb-4" />
-          
+
           <div>
             <button className="bg-[#FF2748] py-4 px-6 rounded-xl hover:scale-105 active:scale-95 transition-transform">
               Place a Bid
             </button>
           </div>
+        </div>
+      </div>
+      <div className="mt-24">
+        <h2 className="text-4xl mt-24">More Works</h2>
+        <div className="flex flex-wrap items-start gap-16 mt-7">
+          {nfts
+            .filter((_nft, index) => index < 3)
+            .map((nft, i) => (
+              <NFTCard key={i} nft={nft} />
+            ))}
         </div>
       </div>
     </div>
