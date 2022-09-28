0199b876946754eea4be1a8e3b4c6a1e1c4a9164
jonathancsr <jonathancsrr@gmail.com>
2022-09-28 12:17:23 -0300

    feat: navbar

---
+++ pages/_navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div className="h-2 px-24 py-10 flex items-center cursor-pointer">
+        <p>Coders NFT</p>
+      </div>
+      <hr className="w-full border-[#242634]"/>
+    </div>
+  )
+}
