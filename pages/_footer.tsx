9a09b7fd634cffaaf2511feb6fd336b44b385a63
jonathancsr <jonathancsrr@gmail.com>
2022-09-28 13:00:30 -0300

    feat: add logo

--- pages/_footer.tsx
+++ pages/_footer.tsx
@@ -1,9 +1,12 @@
+import Logo from "../assets/Logo";
+
 export default function Footer() {
   return (
     <div className="px-24 mt-10 pb-4">
       <hr className="w-full border-[#242634] mt-12"/>
-      <div className="h-16 py-10 flex items-start">
-        <p className="ml-3 text-[#93989a]">Coders NFT. All rights reserved</p>
+      <div className="h-16 py-10 flex items-center">
+        <Logo />
+        <p className="ml-3 text-[#93989a]">Starck NFT. All rights reserved</p>
       </div>
     </div>
   )
--- pages/_navbar.tsx
+++ pages/_navbar.tsx
@@ -1,10 +1,13 @@
+import Logo  from "../assets/Logo";
+
 export default function Navbar() {
   return (
     <div>
       <div className="h-2 px-24 py-10 flex items-center cursor-pointer">
-        <p>Coders NFT</p>
+        <Logo />
+        <p className="ml-3 font-semibold">Starck NFT</p>
       </div>
-      <hr className="w-full border-[#242634]"/>
+      <hr className="w-full border-[#242634]" />
     </div>
-  )
+  );
 }
